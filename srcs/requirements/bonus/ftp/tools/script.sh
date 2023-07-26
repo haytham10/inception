27a439718bb65e6b2ee23043fa7430f98c006728
haytham10 <cnarabi@gmail.com>
2023-07-26 10:03:37 +0100

    Descriptive Comments

--- srcs/requirements/bonus/ftp/tools/script.sh
+++ srcs/requirements/bonus/ftp/tools/script.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
+
+# Create a directory for vsftpd to use as a session directory
 mkdir -p /var/run/vsftpd/empty
+
+# Create a new user with the FTP_USER as the username and /var/www as the home directory
 adduser --home /var/www ${FTP_USER}
+
+# Set the password for the FTP_USER
 echo ${FTP_USER}:${FTP_PASSWORD} | chpasswd
+
+# Add the FTP_USER to the root group to allow access to necessary files
 adduser ${FTP_USER} root
+
+# Start the VSFTPD server with the configuration file /etc/vsftpd.conf
 exec /usr/sbin/vsftpd /etc/vsftpd.conf
--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
+
+# Initialize MySQL data directory
 mysql_install_db
+
+# Start the MariaDB service
 service mariadb start
-if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]
-then
-mysql -u root -e "DROP DATABASE IF EXISTS test;"
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-mysql -u root -e "FLUSH PRIVILEGES;"
+
+# Check if the database already exists; if not, create it and grant privileges
+if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
+    mysql -u root -e "DROP DATABASE IF EXISTS test;"
+    mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+    mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+    mysql -u root -e "FLUSH PRIVILEGES;"
 fi
-mysqladmin shutdown -p${MYSQL_ROOT_PASSWORD}
+
+# Shutdown the MySQL server
+mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
+
+# Start the MySQL server process
 exec mysqld --user=mysql
