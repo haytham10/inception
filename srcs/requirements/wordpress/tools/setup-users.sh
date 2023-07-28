d37f9ac3932b6caaa0cfdae6135cb1ab929168f9
haytham10 <cnarabi@gmail.com>
2023-07-28 02:13:33 +0100

    setup-users script

---
+++ srcs/requirements/wordpress/tools/setup-users.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Set the admin user details from environment variables
+ADMIN_USERNAME="$WP_ADMIN_USER" # Get the admin username from the environment variable
+ADMIN_PASSWORD="$WP_ADMIN_PASS" # Get the admin password from the environment variable
+ADMIN_EMAIL="owner@inception.com" # Replace with your desired email address
+
+# Create a my-plugins directory
+mkdir -p /var/www/html/wp-content/my-plugins
+
+# Create a PHP script to create the admin user dynamically
+cat <<EOF > /var/www/html/wp-content/my-plugins/create-admin-user.php
+<?php
+add_action( 'init', function () {
+    \$username = '$ADMIN_USERNAME';
+    \$password = '$ADMIN_PASSWORD';
+    \$email_address = '$ADMIN_EMAIL';
+
+    if ( ! username_exists( \$username ) ) {
+        \$user_id = wp_create_user( \$username, \$password, \$email_address );
+        \$user = new WP_User( \$user_id );
+        \$user->set_role( 'administrator' );
+    }
+
+} );
+EOF
+
+# Set the correct permissions for the PHP script
+chmod 644 /var/www/html/wp-content/my-plugins/create-admin-user.php
+
+# Set the user1 details from environment variables
+USER1_USERNAME="$WP_USER_NAME" # Get the user1 username from the environment variable
+USER1_PASSWORD="$WP_USER_PASS" # Get the user1 password from the environment variable
+USER1_EMAIL="user1@inception.com" # Replace with your desired email address
+
+# Create a PHP script to create the user1 dynamically
+cat <<EOF > /var/www/html/wp-content/my-plugins/create-user1.php
+<?php
+add_action( 'init', function () {
+    \$username = '$USER1_USERNAME';
+    \$password = '$USER1_PASSWORD';
+    \$email_address = '$USER1_EMAIL';
+
+    if ( ! username_exists( \$username ) ) {
+        \$user_id = wp_create_user( \$username, \$password, \$email_address );
+        \$user = new WP_User( \$user_id );
+        \$user->set_role( 'author' );
+    }
+
+} );
+EOF
+
+# Set the correct permissions for the PHP script
+chmod 644 /var/www/html/wp-content/my-plugins/create-user1.php
+
+# Remove execution permission from the setup script to prevent accidental execution
+chmod -x /var/www/html/wp-content/my-plugins/setup-users.sh
