30a4bb9b16eaf3532e96132b06895c744382cdd6
haytham10 <cnarabi@gmail.com>
2023-07-29 22:16:11 +0100

    final updates

--- srcs/requirements/wordpress/tools/setup-users.sh
+++ srcs/requirements/wordpress/tools/setup-users.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wp core install --url=localhost --title="INCEPTION" --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASS} --admin_email=owner@example.com --allow-root
+wp core install --url=localhost --title="INCEPTION" --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=owner@example.com --allow-root
 
-wp user create ${WP_USER_NAME} user1@example.com --user_pass=${$WP_USER_PASS} --role=author --skip-email --allow-root
+wp user create ${WP_USER_NAME} --user_email=user1@example.com --user_pass=${WP_USER_PASSWORD} --role=author --allow-root
